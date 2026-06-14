f35fd51df152290e6eb3b87b13fa06c21f70726d
samanthakarengera <karengerasamantha@gmail.com>
2026-06-14 13:11:56 +0200

    class location

---
+++ ngoapp/src/main/java/be/ehb/ngoapp/model/Location.java
@@ -0,0 +1,26 @@
+package be.ehb.ngo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String naam;
+    private String adres;
+    private int capaciteit;
+
+    public Location() {
+    }
+
+    public Location(String naam, String adres, int capaciteit) {
+        this.naam = naam;
+        this.adres = adres;
+        this.capaciteit = capaciteit;
+    }
+
+    // getters en setters
+}
