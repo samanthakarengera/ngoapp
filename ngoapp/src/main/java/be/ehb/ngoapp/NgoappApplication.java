5dd4a07f89ee7a635a31d86988e0e10ad49571b3
samanthakarengera <karengerasamantha@gmail.com>
2026-06-14 13:08:50 +0200

    Initial commit

---
+++ ngoapp/src/main/java/be/ehb/ngoapp/NgoappApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.ngoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoappApplication.class, args);
+	}
+
+}
---
+++ ngoapp/src/test/java/be/ehb/ngoapp/NgoappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.ngoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
