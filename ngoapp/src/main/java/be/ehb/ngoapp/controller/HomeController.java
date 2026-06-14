3b3b00f4644b16f9115d6bab00cea7afe85c3f6a
samanthakarengera <karengerasamantha@gmail.com>
2026-06-14 13:26:41 +0200

    html pages + homecontroller

---
+++ ngoapp/src/main/java/be/ehb/ngoapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package be.ehb.ngoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
