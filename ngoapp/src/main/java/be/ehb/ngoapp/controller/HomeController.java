641d4620ee7839c537e8b5c7c58dd14b1a938666
samanthakarengera <karengerasamantha@gmail.com>
2026-06-15 20:54:23 +0200

    fixed homecontroller

--- ngoapp/src/main/java/be/ehb/ngoapp/controller/HomeController.java
+++ ngoapp/src/main/java/be/ehb/ngoapp/controller/HomeController.java
@@ -1,13 +1,24 @@
 package be.ehb.ngoapp.controller;
 
+import be.ehb.ngoapp.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EventRepository eventRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute(
+                "evenementen",
+                eventRepository.findAll());
+
         return "index";
     }
 }
